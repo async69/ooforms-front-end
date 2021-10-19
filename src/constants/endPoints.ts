d27278ea6c056787edb5390411ddb45f9fd7dcf5
Yeabsera <64396141+async69@users.noreply.github.com>
2021-10-19 23:03:59 +0300

    Update endPoints.ts

--- src/constants/endPoints.ts
+++ src/constants/endPoints.ts
@@ -1,3 +1,3 @@
 export default {
-  baseURL: "http://ec2-18-119-125-202.us-east-2.compute.amazonaws.com:4024/graphql"
+  baseURL: "http://ec2-18-224-67-62.us-east-2.compute.amazonaws.com:4024/graphql"
 }
